c764108c5d478540bd8b22c363c013dd393ad212
Kyle Morton <kylemortondevelopment@gmail.com>
2022-07-19 22:29:18 -0500

    init

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Kyle Morton on 7/19/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
