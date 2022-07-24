5fce06c8503c3ee8b0c1ed1f99876d153e70228a
Kyle Morton <kylemortondevelopment@gmail.com>
2022-07-24 10:33:16 -0500

    working on custom label style

---
+++ Scrumdinger/Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,16 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Kyle Morton on 7/24/22.
+//
+
+import SwiftUI;
+
+struct TrailingIconLabelStyle: LabelStyle {
+    
+    func makeBody(configuration: Configuration) -> some View {
+        
+    }
+    
+}
