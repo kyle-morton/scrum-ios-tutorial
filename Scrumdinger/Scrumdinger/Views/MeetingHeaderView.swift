c25e3ef64528c7f1696c98379614e0396b346f9a
Kyle Morton <kylemortondevelopment@gmail.com>
2022-07-30 20:53:04 -0500

    Before hooking up life cycle events, after adding timer

---
+++ Scrumdinger/Scrumdinger/Views/MeetingHeaderView.swift
@@ -0,0 +1,57 @@
+//
+//  MeetingHeaderView.swift
+//  Scrumdinger
+//
+//  Created by Kyle Morton on 7/30/22.
+//
+
+import SwiftUI
+
+struct MeetingHeaderView: View {
+    
+    let secondsElapsed: Int;
+    let secondsRemaining: Int;
+    let theme: Theme;
+    
+    private var totalSeconds: Int {
+        secondsElapsed + secondsRemaining;
+    }
+    private var progress: Double {
+        guard totalSeconds > 0 else { return 1; }
+        return Double(secondsElapsed) / Double(totalSeconds);
+    }
+    private var minutesRemaining: Int {
+        secondsRemaining / 60
+    }
+    
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+                .progressViewStyle(ScrumProgressViewStyle(theme: theme));
+            HStack{
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("\(secondsElapsed)", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                        .labelStyle(.trailingIcon);
+                    Label("\(secondsRemaining)", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+        }
+        .padding([.top, .horizontal])
+        .accessibilityElement(children: .ignore)
+        .accessibilityLabel("Time remaining")
+        .accessibilityValue("\(minutesRemaining) minutes");
+    }
+}
+
+struct MeetingHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingHeaderView(secondsElapsed: 60, secondsRemaining: 180, theme: .bubblegum);
+    }
+}
